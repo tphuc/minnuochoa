4dbd8775157d8f9184f91f587fdf50ed98664480
Tran Bao Phuc <felixtran2000@gmail.com>
2021-10-08 14:42:36 +0700

    _

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRlGRFfLlnMkoYhbsd1q6XHpGjpauoIhs",
-  authDomain: "minnuochoa-c24a8.firebaseapp.com",
-  projectId: "minnuochoa-c24a8",
-  storageBucket: "minnuochoa-c24a8.appspot.com",
-  messagingSenderId: "741356781731",
-  appId: "1:741356781731:web:c7b6c6898a7721ab2fff16",
-  measurementId: "G-XLTBW2XVW3"
+  apiKey: "AIzaSyAk_3AMiobPw-s67gztgHuytgwV02K3gsU",
+  authDomain: "minnuochoa-b25b9.firebaseapp.com",
+  projectId: "minnuochoa-b25b9",
+  storageBucket: "minnuochoa-b25b9.appspot.com",
+  messagingSenderId: "18360930051",
+  appId: "1:18360930051:web:34141b90fa44a1d9b86336",
+  measurementId: "G-M7PLY74J6C"
 };
 
 
