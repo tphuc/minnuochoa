69ac976fb5e80bceb6557ea98c76cb78bab99cc7
Tran Bao Phuc <felixtran2000@gmail.com>
2021-10-14 19:22:59 +0700

    _

--- src/ui/Home/index.js
+++ src/ui/Home/index.js
@@ -24,7 +24,7 @@ const HeroCard = ({ data = { label: "ABC", images: [{ name: "" }] } }) => {
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
-            backgroundImage: `url(${data?.images[0]?.name})`
+            backgroundImage: `url(${data?.images[0]?.url})`
         }}
     >
         <div className='hero-card-overlay'></div>
@@ -46,7 +46,7 @@ export default function Home(props) {
     return <div>
         <div>
             <Grid.Container style={{ minHeight: '100vh' }} direction='row' margin={0} gap={0}   >
-                <Grid xs={24} sm={24} md={15}
+                <Grid xs={24} sm={24} md={24}
                     direction='column'
                     style={{ backgroundRepeat: 'no-repeat', width: "100vw", backgroundSize: "cover", backgroundPosition: "center", backgroundImage: `linear-gradient(rgba(20,23,27,0.82) 40vh, rgba(21, 25, 31, 0.2), rgba(21, 25, 31, 0)), url(https://res.cloudinary.com/minnuochoa-com/image/upload/v1633691914/244973743_2349931545143229_7997632194508024423_n_dmff7e.jpg)` }}
                 >
@@ -60,7 +60,7 @@ export default function Home(props) {
                     </div>
 
                 </Grid>
-                <Grid xs={0} sm={24} md={9} >
+                {/* <Grid xs={0} sm={24} md={9} >
                     <Grid.Container width='100%' wrap='wrap'>
                         {
                             categories?.slice(0, 5)?.map((item, id) => <Grid key={id} xs={0} md={12} style={{ position: "relative", background: '#111' }}>
@@ -91,7 +91,7 @@ export default function Home(props) {
                         </Grid>
 
                     </Grid.Container>
-                </Grid>
+                </Grid> */}
 
             </Grid.Container>
             <Spacer h={2} />
@@ -102,10 +102,9 @@ export default function Home(props) {
                     <Button scale={1.5} onClick={() => history.push('/search')} type='secondary-light'>{item.label}</Button>
                 </div>)}
             </Marquee> */}
-        {isMobile && <Text my={1} style={{ textAlign: "center" }} b h3>Ở ĐÂY MIN CÓ BÁN</Text>}
+        <Text my={1} style={{ textAlign: "center" }} b h3>Ở ĐÂY MIN CÓ BÁN</Text>
 
-            {
-                isMobile && <div style={{ overflowY: "scroll", display: "flex", flexDirection: "row" }}>
+            <div style={{ overflowY: "scroll", cursor:"pointer", display: "flex", flexDirection: "row", justifyContent: isMobile?  'flex-start': "center" }}>
 
                     {
                         categories?.map((item, id) => <div 
@@ -121,14 +120,14 @@ export default function Home(props) {
                                 padding:10
                             }}
                             >
-                                <Image width='100px' height='100px' style={{ borderRadius:"50%"}} src={item.images[0].name} alt=''/>
+                                <Image  width={isMobile ?  '100px' : '160px'} height={isMobile ?  '100px' : '160px'} style={{ borderRadius:"50%"}} src={item.images[0].url} alt=''/>
                                 <Text  style={{  zIndex: 1 }} h5>{item.label}</Text>
                             </div>)
                     }
                     
 
                 </div>
-            }
+            
             <Spacer h={2} />
             <Text my={1} style={{ textAlign: "center" }} b h3>SẢN PHẨM NỔI BẬT</Text>
             {/* <Grid.Container wrap='wrap' direction='row' justify='flex-start' gap={0} margin={0} style={{maxWidth:"100vw"}}  >
@@ -136,7 +135,7 @@ export default function Home(props) {
                     products?.map((item, id) => <Grid xs={12} sm={12} md={3} ><ProductCard data={item} /></Grid>)
                 }
             </Grid.Container> */}
-            <Marquee speed={0} style={{ width: "100%" }} gradient={false}>
+            <Marquee speed={60} style={{ width: "100%" }} gradient={false}>
                 {
                     products?.map((item, id) => <ProductCard width={'160px'} data={item} />)
                 }
