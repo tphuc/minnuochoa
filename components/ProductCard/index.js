5a452827f6af322b324fc3402cc033043bb23dc3
Tran Bao Phuc <felixtran2000@gmail.com>
2022-10-27 12:54:08 +0700

    updat color tags

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -17,11 +17,11 @@ export default function ProductCard({
   const { palette } = useTheme()
   return <Link href={`/product/${data.id}`} passHref={false} width='100%' style={{ display: "flex", width: width, margin: 10, justifyContent: "center", alignItems: "center" }} >
     <div style={{ border: "none", position: "relative", cursor: "pointer", maxWidth: width }} width="100%" >
-      {data?.isNew && <div style={{ position: "absolute", zIndex: 1, top: 0, left: 0, padding: '2px 5px', backgroundColor: "yellow" }}>
-        new
+      {data?.isNew || true && <div style={{ position: "absolute", zIndex: 1, top: 0, left: 0, padding: '2px 5px', color:"white", backgroundColor: "crimson" }}>
+        New
       </div>}
-      {data?.isHot && <div style={{ position: "absolute", zIndex: 1, top: 0, right: 0, color: "white", padding: '2px 5px', backgroundColor: "crimson" }}>
-        hot
+      {data?.isHot || true && <div style={{ position: "absolute", zIndex: 1, top: 0, right: 0, color: "white", padding: '2px 5px', backgroundColor: palette.accents_6 }}>
+        Hot
       </div>}
       <div style={{ width: width, height: height, position: "relative", display: "flex", justifyContent: "center", alignItems: "center" }}>
         <Image objectFit='cover' alt='-' src={data?.images[0].url} layout='fill' />
