08dae96627fcb629f6df335d5401a00b2979cf62
Tran Bao Phuc <felixtran2000@gmail.com>
2022-10-27 11:33:28 +0700

    update new + hot

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -15,15 +15,21 @@ export default function ProductCard({
 }) {
 
   const { palette } = useTheme()
-  return <Link href={`/product/${data.id}`} passHref={false} width='100%' style={{display:"flex", width:width, margin:10, justifyContent:"center", alignItems:"center"}} >
-    <div style={{ border: "none", cursor:"pointer", maxWidth:width  }} width="100%" >
-      <div style={{width:width, height:height, position:"relative", display:"flex", justifyContent:"center", alignItems:"center"}}>
-        <Image objectFit='cover' alt='-' src={data?.images[0].url} layout='fill'/>
-      </div> 
-      <div style={{ padding: 10, paddingTop:0, display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center" }}>
-      <Text style={{width:'100%', textAlign:"center", whiteSpace:"nowrap", overflow:"hidden", textOverflow:'ellipsis', margin:0}} truncate h6 mb={0}>{ data?.brand?.label}</Text>
-        <Text style={{maxWidth:'100%', fontWeight:"300", height:50,  textAlign:"center", overflow:"hidden", textOverflow:'ellipsis', }} truncate margin={0}>{data.label}</Text>
-        {data?.variants?.length && showPrice && <Text  style={{fontSize:14, width:'100%', textAlign:"center", whiteSpace:"nowrap", overflow:"hidden", textOverflow:'ellipsis', color: palette.accents_5}} type="secondary" small>{formatNumber(parseLabelPrice(data.variants[0].label).price)} - {formatNumber( parseLabelPrice(data.variants[data.variants?.length - 1].label).price )}</Text>}
+  return <Link href={`/product/${data.id}`} passHref={false} width='100%' style={{ display: "flex", width: width, margin: 10, justifyContent: "center", alignItems: "center" }} >
+    <div style={{ border: "none", position: "relative", cursor: "pointer", maxWidth: width }} width="100%" >
+      {data?.isNew && <div style={{ position: "absolute", zIndex: 1, top: 0, left: 0, padding: '2px 5px', backgroundColor: "yellow" }}>
+        new
+      </div>}
+      {data?.isHot && <div style={{ position: "absolute", zIndex: 1, top: 0, right: 0, color: "white", padding: '2px 5px', backgroundColor: "crimson" }}>
+        hot
+      </div>}
+      <div style={{ width: width, height: height, position: "relative", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Image objectFit='cover' alt='-' src={data?.images[0].url} layout='fill' />
+      </div>
+      <div style={{ padding: 10, paddingTop: 0, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ width: '100%', textAlign: "center", whiteSpace: "nowrap", overflow: "hidden", textOverflow: 'ellipsis', margin: 0 }} truncate h6 mb={0}>{data?.brand?.label}</Text>
+        <Text style={{ maxWidth: '100%', fontWeight: "300", height: 50, textAlign: "center", overflow: "hidden", textOverflow: 'ellipsis', }} truncate margin={0}>{data.label}</Text>
+        {data?.variants?.length && showPrice && <Text style={{ fontSize: 14, width: '100%', textAlign: "center", whiteSpace: "nowrap", overflow: "hidden", textOverflow: 'ellipsis', color: palette.accents_5 }} type="secondary" small>{formatNumber(parseLabelPrice(data.variants[0].label).price)} - {formatNumber(parseLabelPrice(data.variants[data.variants?.length - 1].label).price)}</Text>}
       </div>
     </div>
   </Link>
--- components/ProductSidebar/index.js
+++ components/ProductSidebar/index.js
@@ -34,38 +34,45 @@ export default function ProductSidebar({
 
     const addToCart = () => {
 
-        let { id, label, images, variants, ...fields} = data
+        let { id, label, images, variants, ...fields } = data
 
         let lineItems = JSON.parse(Cookies.get('cart') || '[]');
-        Cookies.set('cart', JSON.stringify([...lineItems, {id, label, images, variant: variants[activeVariantIndex], amount: 1 }]))
+        Cookies.set('cart', JSON.stringify([...lineItems, { id, label, images, variant: variants[activeVariantIndex], amount: 1 }]))
         cartMutate()
     }
 
-    return <div style={{ position: "relative", width:"100%" }}>
+    return <div style={{ position: "relative", width: "100%" }}>
+        {data?.isNew || true && <div style={{ display: "inline", zIndex: 1, top: 0, left: 0, padding: '2px 5px', backgroundColor: "yellow" }}>
+            new
+        </div>}
+        {data?.isHot || true && <div style={{ display: "inline", zIndex: 1, top: 0, right: 0, color: "white", padding: '2px 5px', backgroundColor: "crimson" }}>
+            hot
+        </div>}
         <Text h2 mt={0}>{data?.label}</Text>
+
         <Text h3>Dung tích</Text>
 
         {
-            data?.variants?.map((item, idx) => <Button key={idx} onClick={() => setActiveVariantIndex(idx)} type={activeVariantIndex == idx ? 'secondary-light' : 'default'} mr={1} auto>{ parseLabelPrice(item?.label).label}</Button>)
+            data?.variants?.map((item, idx) => <Button key={idx} onClick={() => setActiveVariantIndex(idx)} type={activeVariantIndex == idx ? 'secondary-light' : 'default'} mr={1} auto>{parseLabelPrice(item?.label).label}</Button>)
         }
 
         <Spacer h={0.5} />
-        <Text h3 type='success' >{ data?.variants?.[activeVariantIndex]?.label ? formatNumber( parseLabelPrice(data?.variants?.[activeVariantIndex]?.label).price) : '' }</Text>
-        <Divider/>
+        <Text h3 type='success' >{data?.variants?.[activeVariantIndex]?.label ? formatNumber(parseLabelPrice(data?.variants?.[activeVariantIndex]?.label).price) : ''}</Text>
+        <Divider />
         <Description title={<Text marginBottom={0} h5>Xuất xứ</Text>} content={<Text >{data?.origin}</Text>} />
         <Description title={<Text marginBottom={0} h5>Giới tính</Text>} content={<Text >{data?.gender}</Text>} />
         <Description title={<Text marginBottom={0} h5>Phong cách</Text>} content={<Text >{data?.style}</Text>} />
         <Spacer h={1} />
-        <Divider/>
-        <Button 
-        onClick={addToCart} 
-        scale={2} type='secondary-light' iconRight={<ShoppingBag />} width='100%'>Thêm vào giỏ</Button>
-        <Collapse.Group style={{ width:"100%"}}>
-            <Collapse style={{width:"100%"}} title={<Text h4>Giới thiệu</Text>}>
-                <div className='content-description' style={{width:"100%", whiteSpace:'break-spaces'}} dangerouslySetInnerHTML={{__html:data?.introduction}}></div>
+        <Divider />
+        <Button
+            onClick={addToCart}
+            scale={2} type='secondary-light' iconRight={<ShoppingBag />} width='100%'>Thêm vào giỏ</Button>
+        <Collapse.Group style={{ width: "100%" }}>
+            <Collapse style={{ width: "100%" }} title={<Text h4>Giới thiệu</Text>}>
+                <div className='content-description' style={{ width: "100%", whiteSpace: 'break-spaces' }} dangerouslySetInnerHTML={{ __html: data?.introduction }}></div>
             </Collapse>
         </Collapse.Group>
-       
+
 
     </div>
 }
